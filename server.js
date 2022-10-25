31c9144d3ecd128c66f9a2a68a38a5b1f7f13c10
Daniel <62781672+dwernz@users.noreply.github.com>
2022-10-25 15:45:02 -0400

    styling fix, add .net and aws

--- server.js
+++ server.js
@@ -1,50 +1,61 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
-// set up the view engine to ejs
-app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"));
 
-// user res.render to load up an ejs view file
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
 
 // index page
-app.get("/", function(req, res) {
-    res.render('pages/index');
+app.get('/', function(req, res) {
+  res.render('pages/index');
 });
 
 // Frontend Developer
-
+app.get("/frontend", function(req, res) {
+    res.render("pages/frontend");
+});
 
 // Backend Developer
 
 
-// AWS DevOps Engineer
-
-
-// .NET DEveloper
-
-
-// Data Analyst
-
-
-// Director of Information Technology
-
-
-// Vice President of Development
-
-
-// Information Technology Manager
-
-
-// Data Architect
-
+// .NET Developer
+app.get("/netdev", function(req, res) {
+  res.render("pages/netdev");
+});
 
 // QA Engineer
+
+
+// Python Developer
+
+
+// AWS DevOps Engineer
+app.get("/awsdevops", function(req, res) {
+  res.render("pages/AWSDevOps")
+})
+
+// Data Analyst
 
 
 // Data Scientist
 
 
-// Python Developer
+// Data Architect
+
+
+// Data Scientist
+
+
+// Director of IT
+
+
+// Vice President of Development
+
+
+// IT Manager
 
 app.listen(8080);
-console.log("Server is listening on port 8080");
+console.log('Server is listening on port 8080');
