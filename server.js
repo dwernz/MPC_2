37cb8fbdf2d44c66792a28a9992bca52bb3fba2d
Daniel <62781672+dwernz@users.noreply.github.com>
2022-10-28 13:06:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -107,8 +107,7 @@ app.get("/xamarinDev", function(req, res) {
 //
 
 
-
+const PORT = process.env.PORT || 5001;
 
 // Message
-app.listen(8080);
-console.log('Server is listening on port 8080');
+app.listen(PORT, ()=> console.log("Server is listening on port $(PORT)"));
